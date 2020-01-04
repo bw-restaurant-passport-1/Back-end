67ba0fd57197b663bf59b9acc0d0472a7ed9b022
aidenbow <aiden.bowcott@gmail.com>
2020-01-03 17:20:54 -0700

    added passport_model

---
+++ routes/passport_model.js
@@ -0,0 +1,24 @@
+const db = require("../data/dbConfig")
+
+function add(data) {
+    return db("passport").insert(data)
+}
+
+function findByUser(userId) {
+    return db("passport").where("user_id", "=", userId)
+}
+
+function findByRestaurant(restaurantId) {
+    return db("passport").where("restaurant_id", "=", restaurantId)
+}
+
+function findAll() {
+    return db("passport")
+}
+
+module.exports {
+    add,
+    findByUser,
+    findByRestaurant,
+    findAll
+}

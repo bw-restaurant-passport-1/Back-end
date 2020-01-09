b5a4c9425602d58c8fba0a88eaf2a2d0c27bfbd5
AidenBow <AidenBow@users.noreply.github.com>
2020-01-08 21:50:50 -0800

    starter server.spec file

---
+++ server.spec.js
@@ -0,0 +1,26 @@
+const request = require("supertest")
+const server = require("./server.js")
+
+describe("server", () => {
+  describe("index", () => {
+    it("should return 200", async () => {
+      const response = await request(server).get("/");
+
+      expect(response.status).toEqual(200);
+    });
+
+    it("should return a JSON object fron the index route", async () => {
+      const expectedBody = { api: "running" };
+
+      const response = await request(server).get("/");
+
+      expect(response.body).toEqual(expectedBody);
+    });
+
+    it("should return a JSON object fron the index route", async () => {
+      const response = await request(server).get("/");
+
+      expect(response.type).toEqual("application/json");
+    });
+  });
+});

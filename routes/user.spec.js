fcbc3e92bbc6a364466c353df61ca8eb55f5d279
AidenBow <AidenBow@users.noreply.github.com>
2020-01-10 15:53:00 -0800

    login token test

--- routes/user.spec.js
+++ routes/user.spec.js
@@ -1,5 +1,28 @@
 const Users = require("./user_model")
 const db = require("../data/dbConfig")
+const request = require("supertest")
+const server = require("../server")
+
+// beforeAll(async (done) => {
+//   await Users.add({
+//     username: "aiden",
+//     password: "123",
+//     city: "gilbert",
+//     name: "aiden",
+//     email: "aiden@aiden.com"
+//   }) 
+
+//   request(server)
+//   .post("/api/users/login")
+//   .send({
+//     username: "aiden",
+//     password: "123",
+//   })
+//   .end((err, response) => {
+//     token = response.body.token
+//     done()
+//   })
+// })
 
 describe("user_router_model", () => {
   it("should be test environment", () => {
@@ -32,6 +55,37 @@ describe("user_router_model", () => {
       expect(user[0].username).toBe("aiden")
     })
 
+    beforeEach(async () => {
+      await db("users").truncate()
+    })
+  })
+
+  describe("login", () => {
+    it("should return token", async() => {
+      await request(server).post("/api/users/register").send({
+        username: "aiden",
+        password: "123",
+        city: "gilbert",
+        name: "aiden",
+        email: "aiden@aiden.com"
+      })
+
+      const response = await request(server)
+      .post("/api/users/login")
+      .send({username: "aiden", password: "123"})
+
+      expect(response.text).toContain("token")
+    })
+    
+    // test('It responds with JSON', () => {
+    //   return request(server)
+    //     .get('/users/users')
+    //     //.set('Authorization', token)
+    //     .then((response) => {
+    //       expect(response.statusCode).toBe(200);
+    //       expect(response.type).toBe('application/json');
+    //     });
+    // });
     beforeEach(async () => {
       await db("users").truncate()
     })
--- server.spec.js
+++ server.spec.js
@@ -4,23 +4,23 @@ const server = require("./server.js")
 describe("server", () => {
   describe("index", () => {
     it("should return 200", async () => {
-      const response = await request(server).get("/");
+      const response = await request(server).get("/")
 
       expect(response.status).toEqual(200);
     });
 
     it("should return welcome message", async () => {
-      const expectedBody = { message: "welcome" };
+      const expectedBody = { message: "welcome" }
 
-      const response = await request(server).get("/");
+      const response = await request(server).get("/")
 
-      expect(response.body).toEqual(expectedBody);
+      expect(response.body).toEqual(expectedBody)
     });
 
-    it("should return a JSON object fron the index route", async () => {
-      const response = await request(server).get("/");
+    it("should be JSON", async () => {
+      const response = await request(server).get("/")
 
-      expect(response.type).toEqual("application/json");
+      expect(response.type).toEqual("application/json")
     });
   });
 });
